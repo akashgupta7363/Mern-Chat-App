858db1561f6b0878eff5fa59aacd8b33e5d53426
Akash Gupta <akashgupta7363@gmail.com>
2023-04-04 17:51:58 +0530

    Created Login frontend part

--- mern-chat-frontend/src/pages/Login.js
+++ mern-chat-frontend/src/pages/Login.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { Col,Row, Container } from 'react-bootstrap'
+import './Login.css'
+import { Link } from 'react-router-dom';
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+  <Container>
+    <Row>
+      <Col md={5} className="login_bg"></Col>
+      <Col md={7} className="d-flex   algn-iems-center justify-content-center flex-direction-column" >
+
+    <Form style={{width:'80%',maxWidth:500}}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+       Login
+      </Button>
+      <div className='py-4'>
+        <p className='text-center'>Don't have an account ?<Link to="/signup">Signup</Link></p>
+
+      </div>
+    </Form>
+      </Col>
+    </Row>
+    </Container>
+    );
+
 }
 
 export default Login
